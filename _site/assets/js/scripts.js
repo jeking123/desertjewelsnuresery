e1c80e213dabe9c81594a4c8cecc83356659567b
jeremy <>
2019-03-07 21:17:25 -0800

    Updates to index

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -1,3 +1,11 @@
+// adjusting settings for bulma carousel
+// https://creativebulma.net/product/carousel/demo
 window.onload = function() {
-  var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
+  var carousels = bulmaCarousel.attach('.hero-carousel',{
+    autoplay: true,
+    autoplaySpeed: 5000,
+    loop: true,
+    navigation: true,
+    effect: 'fade'
+  }); // carousels now contains an array of all Carousel instances
 };
